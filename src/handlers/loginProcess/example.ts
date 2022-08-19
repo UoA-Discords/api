0b8bf8f4a9af298b2ff0651968ed61216c5f1d2a
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-08-19 21:20:37 +1200

    remove `guilds` scope requirement

--- src/handlers/loginProcess/example.ts
+++ src/handlers/loginProcess/example.ts
@@ -2,6 +2,7 @@ import { OAuth2Routes, OAuth2Scopes } from 'discord-api-types/v10';
 import { randomBytes } from 'crypto';
 import { Config } from '../../global/Config';
 
+// we don't actually require the "Guilds" scope, this is just for the example
 const SCOPES: OAuth2Scopes[] = [OAuth2Scopes.Guilds, OAuth2Scopes.Identify];
 
 /** Creates an example Discord OAuth2 authorization link. */
