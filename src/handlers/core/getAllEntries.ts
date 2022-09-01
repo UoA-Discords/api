d902cc2056e66863d20e44639b1e07d19224c5b4
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-09-01 17:11:49 +1200

    update `/entries` endpoint to include featured

--- src/handlers/core/getAllEntries.ts
+++ src/handlers/core/getAllEntries.ts
@@ -3,5 +3,7 @@ import { EntriesDatabases } from '../../classes/Databases';
 import { EntryStates } from '../../shared/Types/Entries';
 
 export const getAllEntries: RequestHandler = (_req, res) => {
-    return res.status(200).json(EntriesDatabases[EntryStates.Approved].getAll());
+    return res
+        .status(200)
+        .json([...EntriesDatabases[EntryStates.Approved].getAll(), ...EntriesDatabases[EntryStates.Featured].getAll()]);
 };
