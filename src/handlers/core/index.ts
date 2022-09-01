e9015b9ab53a3360a0429518dbf5cf074a23b280
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-09-01 17:12:03 +1200

    update exports from core

--- src/handlers/core/index.ts
+++ src/handlers/core/index.ts
@@ -1,6 +1,5 @@
 export * from './apply';
 export * from './getAllEntries';
 export * from './getEntriesOfState';
-export * from './getFeaturedEntries';
 export * from './getOptOutEntries';
 export * from './getSelfPendingEntries';
